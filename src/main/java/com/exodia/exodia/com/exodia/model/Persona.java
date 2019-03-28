7378c5e5b382fade6b7516ce0de7e736cb9da5bb
AlejoGalloM <AlejoGalloM@users.noreply.github.com>
2019-03-28 13:31:22 -0500

    primer commit
    Took 10 minutes

---
+++ src/main/java/com/exodia/exodia/com/exodia/model/Persona.java
@@ -0,0 +1,17 @@
+package com.exodia.exodia.com.exodia.model;
+
+import sun.util.calendar.BaseCalendar;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+    private BaseCalendar.Date fechaNacimiento;
+
+    public Persona(String nombre, String apellido, String identificacion, BaseCalendar.Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.identificacion = identificacion;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
